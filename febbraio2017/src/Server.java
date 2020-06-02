fd7189f563aaf22740540586c3f8941a6e4bb20f
matteolimoncini <matteo@limoncini.it>
2020-06-02 17:09:13 +0200

    bug fixed bug fixed

--- febbraio2017/src/Server.java
+++ febbraio2017/src/Server.java
@@ -14,83 +14,95 @@ public class Server {
             listatreni[i] = new Treno(i + 1);
         }
 
+        DatagramSocket sToClient = null;
         try {
             byte[] buffer;
             DatagramPacket dpin;
-            buffer = new byte[DIM_BUFFER];
-            dpin = new DatagramPacket(buffer, DIM_BUFFER);
-            DatagramSocket sToClient = new DatagramSocket(0);
+            sToClient = new DatagramSocket(0);
             System.out.println("SERVER: indirizzo: " + sToClient.getLocalAddress() + " porta: " + sToClient.getLocalPort());
-            sToClient.receive(dpin);
-            System.out.println("CLIENT: indirizzo: " + dpin.getAddress().getHostAddress() + " porta: " + dpin.getPort());
-            SocketAddress isaClient = dpin.getSocketAddress();
-
-            messageFromClient = new String(buffer, 0, dpin.getLength());
-            char identita = messageFromClient.charAt(0);
-
-            if (Character.isUpperCase(identita)) {
-                System.out.println("il client è una stazione");
-
+            while (true) {
                 buffer = new byte[DIM_BUFFER];
                 dpin = new DatagramPacket(buffer, DIM_BUFFER);
                 sToClient.receive(dpin);
-                messageFromClient = new String(buffer, 0, dpin.getLength());
-                System.out.println("ho ricevuto dal client "+messageFromClient);
+                System.out.println("CLIENT: indirizzo: " + dpin.getAddress().getHostAddress() + " porta: " + dpin.getPort());
+                SocketAddress isaClient = dpin.getSocketAddress();
 
-                StringTokenizer st = new StringTokenizer(messageFromClient);
-                int positionStation = -1, count = 0, number = -1;
-                String element;
-                while (st.hasMoreTokens() && count < 2) {
-                    count++;
-                    element = st.nextToken();
-                    try {
-                        number = Integer.parseInt(element);
-                    } catch (NumberFormatException e) {
-                        if(element.equals(".")){
-                            System.err.println("stazione chiusa");
-                        }else {
-                            System.err.println("stazione: input errato");
-                            e.printStackTrace();
+                messageFromClient = new String(buffer, 0, dpin.getLength());
+                char identita = messageFromClient.charAt(0);
+
+                if (Character.isUpperCase(identita)) {
+                    System.out.println("il client è una stazione");
+
+                    while (true) {
+                        buffer = new byte[DIM_BUFFER];
+                        dpin = new DatagramPacket(buffer, DIM_BUFFER);
+                        sToClient.receive(dpin);
+                        messageFromClient = new String(buffer, 0, dpin.getLength());
+                        if(messageFromClient.equals(".")){
+                            System.out.println("ho ricevuto dal client un segnale di end");
+                            break;
                         }
-                    }
-                    if (count == 1) {
-                        for (int i = 0; i < listatreni.length; i++) {
-                            if (number == listatreni[i].getCodiceTreno()) {
-                                positionStation = i;
-                                listatreni[i].setIdStazioneUltimoAggiornamento(identita);
+                        System.out.println("ho ricevuto dal client " + messageFromClient);
+
+                        StringTokenizer st = new StringTokenizer(messageFromClient);
+                        int positionStation = -1, count = 0, number = -1;
+                        String element;
+                        while (st.hasMoreTokens() && count < 2) {
+                            count++;
+                            element = st.nextToken();
+                            try {
+                                number = Integer.parseInt(element);
+                            } catch (NumberFormatException e) {
+                                if (element.equals(".")) {
+                                    System.err.println("stazione chiusa");
+                                } else {
+                                    System.err.println("stazione: input errato");
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (count == 1) {
+                                for (int i = 0; i < listatreni.length; i++) {
+                                    if (number == listatreni[i].getCodiceTreno()) {
+                                        positionStation = i;
+                                        listatreni[i].setIdStazioneUltimoAggiornamento(identita);
+                                    }
+                                }
+                            }
+                            if (count == 2) {
+                                listatreni[positionStation].setRitardo(number);
+                                System.out.println("la stazione " + identita + " ha aggiornato il ritardo del treno");
+
+                                buffer = INPUT_STAZIONE_OK.getBytes();
+                                DatagramPacket outDp = new DatagramPacket(buffer, INPUT_STAZIONE_OK.length());
+                                outDp.setSocketAddress(isaClient);
+                                sToClient.send(outDp);
+                                System.out.println("inviato al client input stazione ok");
                             }
                         }
+                        if (count > 2) throw new IOException("LA STAZIONE HA INSERITO ID E RITARDO NON VALIDO");
                     }
-                    if (count == 2) {
-                        listatreni[positionStation].setRitardo(number);
-                        System.out.println("la stazione " + identita + " ha aggiornato il ritardo del treno");
 
-                        buffer = INPUT_STAZIONE_OK.getBytes();
-                        DatagramPacket outDp = new DatagramPacket(buffer,INPUT_STAZIONE_OK.length());
-                        outDp.setSocketAddress(isaClient);
-                        sToClient.send(outDp);
-                        System.out.println("inviato al client input stazione ok");
-                    }
+                } else if (Character.isLowerCase(identita)) {
+
+                    System.out.println("il client è un utente");
+                    buffer = new byte[DIM_BUFFER];
+                    dpin = new DatagramPacket(buffer, DIM_BUFFER);
+
+
+                } else {
+                    throw new IllegalHostException("il client non una stazione o un utente!");
                 }
-                if (count > 2) throw new IOException("LA STAZIONE HA INSERITO ID E RITARDO NON VALIDO");
-
-            } else if (Character.isLowerCase(identita)) {
-
-                System.out.println("il client è un utente");
-                buffer = new byte[DIM_BUFFER];
-                dpin = new DatagramPacket(buffer, DIM_BUFFER);
-
-
-            } else {
-                throw new IllegalHostException("il client non una stazione o un utente!");
             }
-
         } catch (SocketException e) {
             e.printStackTrace();
         } catch (IllegalHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            assert sToClient!=null;
+            sToClient.close();
+            System.err.println("chiusura socket");
         }
 
     }
